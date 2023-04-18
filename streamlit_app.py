91546b94a17993cc002dd2e018df0b87a0a7b370
Thammarat Thamma <thammarat.th@up.ac.th>
2023-04-18 10:10:17 +0700

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,46 @@
 import streamlit as st
 
-st.title("Thammarat's AMA")
+from google.cloud import firestore
+from google.cloud.firestore import Client
 
-def send_message(name, message, is_private):
-    if is_private:
-        st.write("Your message has been sent privately to Thammarat.")
-    else:
-        st.write("Your message has been sent to Thammarat.")
+@st.cache_resource
+def get_db():
+    db = firestore.Client.from_service_account_json("key.json")
+    return db
+
+def post_message(db: Client, name, message, is_private):
+    payload = {"name": name, "message": message, "is_private": is_private}
+    doc_ref = db.collection("messages").document()
+    doc_ref.set(payload)
+    return doc_ref.id
     
 
+def main():
+    st.title("Thammarat's AMA")
+    st.write("Ask me anything!")
+    
+    db = get_db()
+    
+    with st.expander("Get all messages"):
+        posts_ref = db.collection('messages')
+        
+        for doc in posts_ref.stream():
+            # st.write("This id is: ", doc.id)
+            st.write(doc.to_dict())
 
-with st.expander("Checklist"):
-    st.checkbox("Form to add message")
-    st.checkbox("Submit message")
-    st.checkbox("Read message")
+    
+    with st.form("add message"):
+        input_name = st.text_input("Your name (Optional)", help="can be anonymous")
+        input_message = st.text_area("Your question")
+        is_private = st.checkbox("Hide your message from the public board")
+        submit = st.form_submit_button("Submit")
 
-with st.form("add message"):
-    input_name = st.text_input("Your name (Optional)", help="can be anonymous")
-    input_message = st.text_area("Your question")
-    is_private = st.checkbox("Hide your message from the public board")
-    submit = st.form_submit_button("Submit")
-
-    if submit:
-        send_message(input_name, input_message, is_private)
+        if submit:
+            post_message(db, input_name, input_message, is_private)
+            st.success("Your message has been sent!")
+            st.balloons()
+            
+if __name__ == "__main__":
+    st.set_page_config(page_title="Thammarat's AMA", page_icon=":smile:")
+    main()
+    
